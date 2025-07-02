3bcca3f5e2e0cef225fdb11460547d0127be79dc
Henrique Mendes Ferro <48026099+Ostwick@users.noreply.github.com>
2025-07-02 07:58:44 -0300

    Update api.js

--- backend/api.js
+++ backend/api.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const { insertMensagem } = require('./postgres');
 
-app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.url}`);
+  next();
+});
 
 app.post('/api/mensagens', async (req, res) => {
   try {
