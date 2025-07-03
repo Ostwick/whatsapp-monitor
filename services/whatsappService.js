c875d324a5d517c762d266685191ce2857592741
Henrique Mendes Ferro <48026099+Ostwick@users.noreply.github.com>
2025-07-03 10:07:51 -0300

    Update whatsappService.js

--- services/whatsappService.js
+++ services/whatsappService.js
@@ -82,7 +82,6 @@ class WhatsAppService {
         args: [
           '--no-sandbox',
           '--disable-setuid-sandbox',
-          '--disable-dev-shm-usage',
           '--disable-gpu',
           '--single-process',
           '--no-zygote',
