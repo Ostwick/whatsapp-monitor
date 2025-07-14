ed4b2569dcb62a54c582d6eb8167cb78d0d3a114
Henrique Mendes Ferro <48026099+Ostwick@users.noreply.github.com>
2025-07-14 10:06:59 -0300

    Update whatsappService.js reomved single process for stability

--- services/whatsappService.js
+++ services/whatsappService.js
@@ -59,7 +59,7 @@ function initializeClient(sessionId) {
             args: [
               '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage',
               '--disable-accelerated-2d-canvas', '--no-first-run', '--no-zygote',
-              '--single-process', '--disable-gpu'
+              '--disable-gpu'
             ]
           }
     });
